7d7b6924726b1fb8d4d19fa4b280fecfc3e55b4c
louisslnn <louis.salanon7@gmail.com>
2025-09-03 19:49:48 -0400

    Added demo

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ from collections import namedtuple
 import numpy as np
 
 pygame.init()
-font = pygame.font.Font('arial.ttf', 25)
+font = pygame.font.Font('Snake-AI-agent/arial.ttf', 25)
 #font = pygame.font.SysFont('arial', 25)
 
 #reset
